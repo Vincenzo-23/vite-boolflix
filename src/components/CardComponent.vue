03eee927c1de9e0babfc5a4d541befb4a09a55e9
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-04-16 15:33:43 +0200

    ADD: added the logic to show the flag for the language of the movie inside the CardComponent

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,26 +1,47 @@
 <script>
   export default {
     props: ["card"],
+    data(){
+        return{
+        
+        }
+    }
   }
 </script>
 
 
 <template>
-  <div class="title">
-    Titolo: {{ card.title }}
-  </div>
-  <div class="original_title">
-    Titolo originale: {{ card.original_title }}
-  </div>
-  <div class="language">
-    Lingua originale: {{ card.original_language }}
-  </div>
-  <div class="vote">
-    Voto: {{ card.vote_average }}
-  </div>
+    <div class="card h-100">
+        <div class="title">
+          Titolo: {{ card.title }}
+        </div>
+        <div class="original_title">
+          Titolo originale: {{ card.original_title }}
+        </div>
+        <div class="language">
+          Lingua originale:
+          <span v-if="card.original_language === 'en'">
+              <img src="/enflag.jpg" alt="flag image" class="flag">
+          </span>
+          <span v-else-if="card.original_language === 'it'">
+              <img src="/itflag.png" alt="flag image" class="flag">
+          </span>
+          <span v-else-if="card.original_language === 'fr'">
+              <img src="/frflag.jpg" alt="flag image" class="flag">
+          </span>
+          <span v-else>
+              {{ card.original_language }}
+          </span>
+        </div>
+        <div class="vote">
+          Voto: {{ card.vote_average }}
+        </div>
+    </div>
 </template>
 
 
 <style lang="scss" scoped>
-
+.flag{
+    max-width: 30px;
+}
 </style>
